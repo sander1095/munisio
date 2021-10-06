387b837b6e7ccb0b00547581e4a76199a43bf347
Sander ten Brinke <s.tenbrinke@arcady.nl>
2021-10-06 19:50:24 +0200

    Created a basic project structure

---
+++ test/Munisio.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Munisio.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
