9eb23352211cb0824412a1e08c9578a38b38e5b7
Sander ten Brinke <s.tenbrinke2@gmail.com>
2022-10-19 19:44:39 +0200

    Add some documentation to the new methods

--- src/Munisio/ServiceCollectionExtensions.cs
+++ src/Munisio/ServiceCollectionExtensions.cs
@@ -10,7 +10,8 @@ namespace Munisio
     public static class ServiceCollectionExtensions
     {
         /// <summary>
-        /// Adds Minusio's HATEOAS middleware to the MVC pipeline.
+        /// Adds Munisio's HATEOAS middleware to the MVC pipeline.
+        /// Don't forget to call <see cref="AddHateoasProviders(IServiceCollection)"/> or <see cref="AddHateoasProviders(IServiceCollection, Assembly[])"/> afterwards if you want Munisio to automatically register your HATEOAS providers!
         /// </summary>
         public static MvcOptions AddHateoas(this MvcOptions builder)
         {
@@ -18,9 +19,27 @@ namespace Munisio
             return builder;
         }
 
-        public static IServiceCollection AddHateoas(this IServiceCollection services) => services.AddHateoas(Assembly.GetExecutingAssembly());
+        /// <summary>
+        /// Searches for your <see cref="IHateoasProvider{TModel}"/> and <see cref="IAsyncHateoasProvider{TModel}"/> implementations in your current assembly and registers them in a transient manner to the <paramref name="services"/> you pass in.
+        /// Don't forget to call <see cref="AddHateoas(MvcOptions)"/> so your providers will get executed at runtime!
+        /// </summary>
+        /// <param name="services"></param>
+        /// <remarks>
+        /// Calling this method is not required; it's simply convenient. If you don't want Munisio to search and register your implementations automatically, feel free to register them yourself!
+        /// </remarks>
+        public static IServiceCollection AddHateoasProviders(this IServiceCollection services) => services.AddHateoasProviders(Assembly.GetExecutingAssembly());
 
-        public static IServiceCollection AddHateoas(this IServiceCollection services, params Assembly[] assemblies)
+        /// <summary>
+        /// Searches for your <see cref="IHateoasProvider{TModel}"/> and <see cref="IAsyncHateoasProvider{TModel}"/> implementations in the <paramref name="assemblies"/> you pass in to this method. 
+        /// They will be registered transient manner to the <paramref name="services"/> you pass in.
+        /// 
+        /// Don't forget to call <see cref="AddHateoas(MvcOptions)"/> so your providers will get executed at runtime!
+        /// </summary>
+        /// <param name="services"></param>
+        /// <remarks>
+        /// Calling this method is not required; it's simply convenient. If you don't want Munisio to search and register your implementations automatically, feel free to register them yourself!
+        /// </remarks>
+        public static IServiceCollection AddHateoasProviders(this IServiceCollection services, params Assembly[] assemblies)
         {
             if (assemblies.Length == 0)
             {
@@ -30,10 +49,7 @@ namespace Munisio
             var providedAssemblies = assemblies.Distinct()
                                                .ToArray();
 
-            var targetHateoasInterfaces = new Type[] {
-                typeof(IHateoasProvider<>)
-                , typeof(IAsyncHateoasProvider<>)
-            };
+            var targetHateoasInterfaces = new Type[] { typeof(IHateoasProvider<>), typeof(IAsyncHateoasProvider<>) };
 
             foreach (var targetHateoasInterface in targetHateoasInterfaces)
             {
@@ -57,9 +73,7 @@ namespace Munisio
         {
             if (type is null) yield break;
 
-            foreach (var interfaceType in type.GetInterfaces()
-                                              .Where(type => type.GetTypeInfo().IsGenericType
-                                                                   && type.GetGenericTypeDefinition() == targetType))
+            foreach (var interfaceType in type.GetInterfaces().Where(type => type.GetTypeInfo().IsGenericType && type.GetGenericTypeDefinition() == targetType))
             {
                 yield return interfaceType;
             }
