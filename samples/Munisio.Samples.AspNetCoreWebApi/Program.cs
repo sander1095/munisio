44c13f41eb4de97763261ad39945cb1fccd886a8
Sander ten Brinke <s.tenbrinke2@gmail.com>
2022-10-20 10:23:29 +0200

    Enable munisio in the sample project

--- samples/Munisio.Samples.AspNetCoreWebApi/Program.cs
+++ samples/Munisio.Samples.AspNetCoreWebApi/Program.cs
@@ -1,16 +1,21 @@
-﻿using Munisio.Samples.AspNetCoreWebApi.Database;
+﻿using Munisio;
+using Munisio.Samples.AspNetCoreWebApi.Database;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(x => x.AddHateoas()); // 1: Call AddHateoas!
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddSingleton<UserDatabase>();
 builder.Services.AddSingleton<TweetDatabase>();
+
+builder.Services.AddHateoasProviders(); // 2: Let munisio search and register your providers automatically OR
+//services.AddTransient<IHateoasProvider<Tweet>, TweetHateoasProvider>(); // 3: Do it yourself!
 
 var app = builder.Build();
 
